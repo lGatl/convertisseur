170b1d406f281501e8e29b341f250eb5dede2b83
lGatl <gat55@live.fr>
2016-12-10 03:46:27 +0100

    ajout de GatJs ^^ + commentaires detailés!!!

--- script.js
+++ script.js
@@ -1,115 +1,104 @@
-conv={val:0,tva:0, unit:"HT", rslt:""};
-i=0;
+conv = {val:0 ,tva:0, unit:"HT", rslt:""};/*contiendra toutes les données*/
+i = 0;/*comptera le nombre de calcul*/
+
+var G = function(param){/*pour selectionner des elements plus rapidement*/
+   var premier_caractere, element;
+   var premier_caractere = param.substring(0, 1);/*isole le premier caractere*/
+    premier_caractere == "#" ? element=document.getElementById(param.substring(1,param.length)) :/*si# getelementbyid*/
+    premier_caractere == "." ? element=document.getElementByClassName(param.substring(1,param.length)) :/*si. getelementsbyclassname ATTENTION TABLEAU*/
+    element = document.getElementsByTagName(param);/*si "" getelementsbytagname ATTENTION TABLEAU*/
+return element;
+};
 
 
-
-
-
-
-
-
-
-bt=document.getElementById('bt')
-bt.addEventListener('click', function(e){
+G("#bt").addEventListener('click', function(e){/*Lorsque l'on clique sur le bouton*/
     e.preventDefault();
 
-
-      conv=mev(conv)
-      if(isNaN(conv.tva)||isNaN(conv.val)||conv.tva>100||conv.tva<0||conv.valeur<0){
-        alert("Verifiez votre saisie")
+      conv = mev(conv)/*Appelle la fonction mev =>pour mettre les données en variable objet parametre:objet hors de la fonction*/
+      if(isNaN(conv.tva) || isNaN(conv.val) || conv.tva>100 || conv.tva<0 || conv.valeur<0){/*Si valeur erronnée=>verifiez saisie*/
+        alert("Verifiez votre saisie");
     }else{
       i++
-      aff(conv,i)
-            document.getElementById('tva').value=""
-            document.getElementById('val').value=""
-      }
+      aff(conv,i)/*appelle aff pour afficher les données + precise le nombre de calcul*/
+            G('#tva').value = G('#val').value = "";/*vide les données saisies*/
+      };
+});
 
+function mev(){/*met les données en variable et retourne un objet */
+  var obj = {};
 
-})
+obj.tva   = parseFloat(G('#tva').value);/*donnée traduites en nb*/
+obj.val   = parseFloat(G('#val').value);/*donnée traduites en nb*/
+obj.unit  = G('#unit').value;
+obj.reslt = calc(obj);/*lance le calcul et stoque le resultat*/
 
-
-function mev(){
-  obj={}
- /* quoi faire si clique*/
-obj.tva=parseFloat(document.getElementById('tva').value);
-obj.val=parseFloat(document.getElementById('val').value);
-obj.unit=document.getElementById('unit').value;
-obj.reslt=calc(obj);
-
-return obj
+return obj;
 };
 
-function calc(obj){
-/*CALCULS*/
-  if(obj.unit=="HT"){
-    res = ((obj.val*obj.tva)/100) + obj.val
-    reslt= res.toFixed(2) + " TTC"
-
+function calc(obj){/*CALCULS en fonction de l'objet qui contien les données*/
+  var res, reslt;
+  if(obj.unit == "HT"){/*verifie l'unitée*/
+    res = ((obj.val*obj.tva)/100) + obj.val;
+    reslt = res.toFixed(2) + " TTC";/*arrondi en gerant le nombre de chiffre aprés la virgule+ajoute l'unitée*/
   }else{
-    res=(obj.val*100)/(100+obj.tva)
-    reslt = res.toFixed(2) + " HT"
-  }
-  return reslt
+    res=(obj.val*100)/(100+obj.tva);
+    reslt = res.toFixed(2) + " HT"/*arrondi en gerant le nombre de chiffre aprés la virgule+ajoute l'unitée*/
+  };
+  return reslt;
 
 };
 
-function aff(conv,i){
-resultats=document.getElementById('resultats');
+function aff(conv,i){ /*sert à afficher le resultat + besoin du nombre de calcul et de l'objet contenant les données*/
 
-maDiv = document.createElement("div");
-maDiv.id = i;
-maDiv.className = 'row rst1';
-if(i==1){
-resultats.appendChild(maDiv);
+var maDiv = document.createElement("div"); /*cree une nouvelle div qui contiendra 4 div du resultat*/
+maDiv.id = i; /*donne le nombre de calcul à l'id de la nouvelle div*/
+maDiv.className = 'row rst1';/*lui affecte une class pour qu'elle soit jolie*/
+if( i== 1){
+G('#resultats').appendChild(maDiv);/*insert le premier calcul dans la div prevue à cet effet*/
 }else{
 
-divim=document.getElementById(i-1);
-console.log(divim)
-divim.className = 'row rst';
- resultats.insertBefore(maDiv, divim);
+  G("#"+(i-1)).className = 'row rst'; /*change la classe de la div precedente pour qu'elle soit moin jolie que la premiere*/
+  G('#resultats').insertBefore(maDiv, G("#"+(i-1)));/*insert la nouvelle div avant la div precedente*/
+};
+
+/*cree 4 div chacune contiendra un element de l'objet; la premiere le nombre de calcul*/
+/*PARAMETRES
+: texte a afficher|||nombre de calcul|||le nombre a mettre pour col-xs|||la classe qui permet de faire disparaitre la colonne la moins utile pour le coté responsive*/
+cont(i,i,0," r")/*cree la div qui affiche le nombre de calcul*/
+cont(conv.tva,i,4,"")/*cree la div qui affiche le taux de la tva*/
+cont(conv.val+" "+conv.unit,i,4,"")/*cree la div qui affiche la valeur a convertir*/
+cont(conv.reslt,i,4,"")/*cree la div qui affiche la valeur convertie*/
+}
+/*cree chaque une div a afficher dans une colonne bootstrap*/
+/*PARAMETRES
+: texte a afficher|||nombre de calcul|||le nombre a mettre pour col-xs|||la classe qui permet de faire disparaitre la colonne la moins utile pour le coté responsive*/
+function cont(txt,i,c1,r){
+
+var maDiv = document.createElement("div");/*cree une div*/
+maDiv.className = "col-xs-"+c1+" col-sm-"+3+r /*affecte les class bootstrap*/
+maDiv.innerHTML = txt;/*rempli la div avec le texte a afficher*/
+G('#'+i).appendChild(maDiv);/*insert la nouvelle div dans la div row prevue à cet effet*/
 }
 
-
-cont(i,i,0,3," r")
-cont(conv.tva,i,4,3,"")
-cont(conv.val+" "+conv.unit,i,4,3,"")
-/*cont(conv.unit,i,3,3,"")*/
-cont(conv.reslt,i,4,3,"")
-}
-
-function cont(txt,i,c1,c2,r){
-
-divi=document.getElementById(i);
-maDiv = document.createElement("div");
-/*maDiv.id = i+"_"+n;*/
-maDiv.className = "col-xs-"+c1+" col-sm-"+c2+r
-
-maDiv.innerHTML = txt;
-divi.appendChild(maDiv);
-}
-
+/*_____________________________________________________________________________*/
+/*Ajoute un petit konamikode pour la rigolade*/
 if ( window.addEventListener ) {
 var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
 window.addEventListener("keydown", function(e){
 kkeys.push( e.keyCode );
 if ( kkeys.toString().indexOf( konami ) >= 0 ) {
-
-alert("voici le numero de ma carte banquaire")
-
-alert("naaannn j'deconne")
-alert("salut!")
-alert("c'est encore moi")
-alert("la tu te demende si tu ne vas pas desactiver les poppups")
-alert("mais ta curiosité est trop grande!!!")
-alert("pour decouvrir au final")
-alert("qu'il n'y a rien de spé à la fin :p")
-
-
-kkeys=[]
-
-}
+kkeys = []/*important de vider la liste d'evenements pour que le convertisseur soit toujours utilisable ;) */
+alert("voici le numero de ma carte banquaire");
+alert("naaannn j'deconne");
+alert("salut!");
+alert("c'est encore moi");
+alert("la tu te demende si tu ne vas pas desactiver les poppups");
+alert("mais ta curiosité est trop grande!!!");
+alert("pour decouvrir au final");
+alert("qu'il n'y a rien de spé à la fin :p");
+};
 }, true);
-}
+};
 
 
 
